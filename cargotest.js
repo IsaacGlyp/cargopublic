19e96a63539def4eee0512c943823320d89df0ed
IsaacGlyp <90610839+IsaacGlyp@users.noreply.github.com>
2025-08-04 22:53:08 +0200

    Update cargotest.js

--- cargotest.js
+++ cargotest.js
@@ -257,6 +257,7 @@ function handleMutations(mutationsList, observer) {
                             const img = document.createElement('img');
                             const id = this.id;
                             img.src = imageMap[id] || 'https://isaacglyp.github.io/cargopublic/img/fallback.png'; // fallback als ID niet in map zit
+                            img.style.width = '100%';
                             this.appendChild(img);
                         }
                     });
@@ -302,6 +303,7 @@ observer.observe(parentAnchor, observerConfig);
 
 logColor();
 let myIntervalID = setInterval(runnerFunc, 1000);
+
 
 
 
