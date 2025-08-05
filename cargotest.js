6ff8730e551b6b5e395d8d11a7a95691f541a0b8
IsaacGlyp <90610839+IsaacGlyp@users.noreply.github.com>
2025-08-05 10:51:09 +0200

    Update cargotest.js

--- cargotest.js
+++ cargotest.js
@@ -56,8 +56,8 @@ var  titAr = [
 let titCount = 1;
 let amountOfTits = titAr.length-1;
 let counter = 0;
-let maxTitleLength = window.innerWidth <= 768 ? 500 : 10000;
-let intervalAnimator = 100;
+let maxTitleLength = window.innerWidth <= 768 ? 1000 : 10000;
+let intervalAnimator = window.innerWidth <= 768 ? 500 : 100;
 
 let runnerFunc = (() => {
 
@@ -322,6 +322,7 @@ observer.observe(parentAnchor, observerConfig);
 
 logColor();
 let myIntervalID = setInterval(runnerFunc, 1000);
+
 
 
 
