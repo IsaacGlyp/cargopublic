8e28d3e4cac37cc2403150b8430d58043823512e
IsaacGlyp <90610839+IsaacGlyp@users.noreply.github.com>
2025-12-06 22:02:27 +0100

    Update cargotest.js

--- cargotest.js
+++ cargotest.js
@@ -12,7 +12,7 @@ document.addEventListener("DOMContentLoaded", function() {
       mobileName.style.display = "inline";
 
       const moji = ["@", "A", "∂", "𝒶", "a", "ᵃ", "Ⓐ"];
-      const name = "ISAAC VAN DEN AKER";
+      const name = "ISAAC<br>VAN DEN AKER";
 
       setInterval(() => {
         mobileName.innerHTML = name.replace(/a/gi, () => moji[Math.floor(Math.random() * moji.length)]);
@@ -652,6 +652,7 @@ document.addEventListener('click', () => {
 // ==============================================================================
 // 🎨 IMAGE BRUSH CODE END
 // ==============================================================================
+
 
 
 
